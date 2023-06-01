f9cb557d6518c35b75c55276a302dc43c4990c39
MASTERGENERAL <maliyandrey007@gmail,com>
2023-06-01 12:58:53 +0300

    Дз

---
+++ src/Myarea/Main.java
@@ -0,0 +1,12 @@
+package Myarea;
+
+public class Main {
+    public static void main(String[] args) {
+        double radius = 5.0;
+        double area = MyArea.areaOfCircle(radius);
+
+        System.out.println("Радіус: " + radius);
+        System.out.println("Площа кола: " + area);
+    }
+}
+
---
+++ src/Myarea/MyArea.java
@@ -0,0 +1,10 @@
+package Myarea;
+
+public class MyArea {
+    public static final double PI = 3.14;
+
+    public static double areaOfCircle(double radius) {
+        return PI * radius * radius;
+    }
+}
+
